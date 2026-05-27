a796ed919b221a500faada49868a5fee8f1ccab3
karthik <karthik@gmail.com>
2026-05-27 18:48:40 +0530

    Add project files

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from './mongooseMock.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI is not set');
-    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
